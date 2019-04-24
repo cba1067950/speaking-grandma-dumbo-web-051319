e84508b98ce4422fc9edebf7385b489e35bbcdd8
Joe Jamsky <cba1067950@yahoo.com>
2019-04-24 17:29:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   else
-    return 
 end
